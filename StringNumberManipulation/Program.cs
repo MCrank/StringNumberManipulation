5298499660935eca8674403b5df4dd451670a36f
MCrank <marco_mcse@hotmail.com>
2019-03-02 09:57:51 -0600

    Working number calc

--- StringNumberManipulation/Program.cs
+++ StringNumberManipulation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StringNumberManipulation
 {
@@ -6,7 +7,36 @@ namespace StringNumberManipulation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a comma seperated list of numbers to multiply?");
+            
+
+            var usrInput = (Console.ReadLine().Split(','));
+            //TODO: Validate the user input so stuff doesn't blow up
+
+            List<int> usrInputList = new List<int>();
+            //int[] usrInputArray = new int[usrInput.Length];
+            foreach (string value in usrInput)
+            {
+                usrInputList.Add(Convert.ToInt32(value));
+            }
+
+            var multiplyResults = MultiplyNumbers(usrInputList);
+
+            Console.WriteLine($"\nThe answer is: {multiplyResults}");
+            Console.WriteLine("\nPress any to exit!");
+            Console.ReadLine();
+            //var answer = MultiplyNumbers(usrInput);
+            
+        }
+
+        static int MultiplyNumbers(List<int>numbers)
+        {
+            var results = 1;
+            foreach (int number in numbers)
+            {
+                results = results * number;
+            }
+            return results;
         }
     }
 }
