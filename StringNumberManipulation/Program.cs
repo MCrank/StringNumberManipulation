898e0364bc14765dada3b9747b27f13b34ac4730
MCrank <marco_mcse@hotmail.com>
2019-03-02 09:12:17 -0600

    Add project files.

---
+++ StringNumberManipulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringNumberManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
